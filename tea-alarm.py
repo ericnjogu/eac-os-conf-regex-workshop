b3d67880c42511cd083adb1dfc2e7b96add3e5d9
Eric Njogu <kunadawa@gmail.com>
2019-08-29 12:58:56 +0300

    chore: experimenting with regular expressions in python

---
+++ tea-alarm.py
@@ -0,0 +1,19 @@
+import re
+
+time = "hh:mm"
+
+match = re.match("\d:\d", time)
+
+# change time to 9:11, show the entire match
+match == None
+
+# change pattern to match the minutes
+
+
+# Change hour to double digit and match
+
+
+# introduce groups for hour, minutes
+
+
+# validate that hour is between 9 and 11
